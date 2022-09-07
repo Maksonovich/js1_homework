2d26ca7d16adbe88680a520686dd4e7133c379d9
Maksonovich <kaverron1@gmail.com>
2022-09-07 13:19:00 +0300

    поменял местами знаки <>, названия больше на длине

--- check.js
+++ check.js
@@ -1,9 +1,9 @@
 const num = prompt ('Введите числа?')
 const num2 = prompt ('Введите число?')
-if (num>num2) {
-    alert(num +' больше ' + num2);
-}else if(num<num2) {
-    alert(num+' короче '+num2)
+if (num<num2) {
+    alert(num +' короче ' + num2);
+}else if(num>num2) {
+    alert(num+' длинее '+num2)
 }else{
     alert(" равны ");
 }
